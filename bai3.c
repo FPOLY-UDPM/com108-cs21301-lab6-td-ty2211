ceb94f1cd3ddc93134d3e4586cb5cf12d0b67fbc
td-ty221 <dannh8866@gmail.com>
2026-02-07 08:56:28 +0700

    okay

--- bai3.c
+++ bai3.c
@@ -1,12 +1,43 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Thanh Danh]
+ * MSSV:      [PS48014]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 3: SẮP XẾP MẢNG THEO THỨ TỪ GIẢM DẦN 
 //  Input: Nhập vào từ bàn phím 1 mảng các số nguyên. Mảng gồm n phần tử 
 //  Output: Xuất ra màn hình kết quả mảng đã sắp xếp 
 
-// VIẾT CODE Ở ĐÂY
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    int a[100];
+    printf("Nhap so phan tu (<=100): ");
+    scanf("%d", &n);
+    if(n <= 0 || n > 100){
+        printf("So phan tu khong hop le!");
+        return 0;
+    }
+    for(int i = 0; i < n; i++){
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(a[i] < a[j]){  
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("\nMang sau khi sap xep giam dan:\n");
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
 
--- bai4.c
+++ bai4.c
@@ -1,12 +1,49 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Thanh Danh]
+ * MSSV:      [PS48014]
+ * Lớp:       [COM108]
  *****************************************************************************/
 
 //  BÀI 4: TÍNH BÌNH PHƯƠNG CÁC PHẦN TỬ TRONG MẢNG 2 CHIỀU 
 //  Input: Nhập vào từ bàn phím 1 ma trận các số nguyên. Mảng gồm n hàng, m cột 
 //  Output: Xuất ra màn hình ma trận bình phương 
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+
+int main(){
+    int n, m;
+    int a[100][100];
+
+    printf("Nhap so hang n: ");
+    scanf("%d", &n);
+
+    printf("Nhap so cot m: ");
+    scanf("%d", &m);
+
+    if(n <= 0 || m <= 0 || n > 100 || m > 100){
+        printf("Kich thuoc khong hop le!");
+        return 0;
+    }
+    printf("Nhap ma tran:\n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            a[i][j] = a[i][j] * a[i][j];
+        }
+    }
+    printf("\nMa tran sau khi binh phuong:\n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%5d", a[i][j]); 
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
